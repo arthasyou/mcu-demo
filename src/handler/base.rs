3243e25f8c705ebb5f903d2eae8359ded5711458
Ancient <gamesworldcraft@gmail.com>
2025-12-11 16:17:09 +0800

    hello world

--- src/handler/base.rs
+++
@@ -1,8 +0,0 @@
-use crate::{Event, handler::Handler};
-
-pub struct BaseHandler;
-
-impl Handler for BaseHandler {
-    fn on_event(&mut self, event: Event) {}
-    fn poll(&mut self, delta_ms: u32) {}
-}
